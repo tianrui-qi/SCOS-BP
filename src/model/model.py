5995a42613b95a74750b00e170265b0320ae3089
Tianrui Qi <tianrui.qi@icloud.com>
2025-10-08 02:14:04 -0400

    Update SCOST model output logic

--- src/model/model.py
+++ src/model/model.py
@@ -1,10 +1,12 @@
 import torch
 
-import src.model.tokenizer
-import src.model.masking
-import src.model.embedding
-import src.model.transformer
-import src.model.head
+from typing import Literal
+
+from .tokenizer import Tokenizer
+from .masking import Masking
+from .embedding import Embedding
+from .transformer import Transformer
+from .head import HeadReconstruction, HeadRegression
 
 
 __all__ = ["SCOST"]
@@ -16,21 +18,23 @@ class SCOST(torch.nn.Module):
         num_layers: int, nhead: int, dim_feedforward: int, out_dim: int = 2,
     ) -> None:
         super().__init__()
-        self.tokenizer = src.model.tokenizer.Tokenizer(S, stride)
-        self.masking = src.model.masking.Masking()
-        self.embedding = src.model.embedding.Embedding(D, S, C_max, L_max)
-        self.transformer = src.model.transformer.Transformer(
+        self.tokenizer = Tokenizer(S, stride)
+        self.masking = Masking()
+        self.embedding: torch.nn.Module = Embedding(D, S, C_max, L_max)
+        self.transformer: torch.nn.Module = Transformer(
             D, num_layers, nhead, dim_feedforward
         )
-        self.head_reconstruction = src.model.head.HeadReconstruction(D, S)
-        self.head_regression = src.model.head.HeadRegression(D, out_dim)
+        self.head_reconstruction: torch.nn.Module = HeadReconstruction(D, S)
+        self.head_regression: torch.nn.Module = HeadRegression(D, out_dim)
 
     def forward(
         self, x: torch.Tensor,      # (B, C, T)
         channel_idx: torch.Tensor,  # (B, C)
-    ) -> torch.Tensor | tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        token = self.tokenizer(x)   # (B, C, T) -> (B, C, L, S)
-        if self.training:
+        mask: bool = False, 
+        task: Literal["regression", "reconstruction"] = "regression"
+    ) -> tuple[torch.Tensor, torch.Tensor | None, torch.Tensor | None]:
+        token = self.tokenizer.forward(x)   # (B, C, T) -> (B, C, L, S)
+        if mask: 
             mlm_mask, mlm_mode, mlm_rand = self.masking(token)
         else:
             mlm_mask, mlm_mode, mlm_rand = None, None, None
@@ -39,12 +43,20 @@ class SCOST(torch.nn.Module):
         B, C, L, D = x.shape
         x = x.reshape((B, C*L, D))  # (B, C, L, D) -> (B, C*L, D)
         x = self.transformer(x)     # (B, C*L, D) -> (B, C*L, D)
-        if self.training:
+        if task not in ["regression", "reconstruction"]:
+            raise ValueError("task must be 'regression' or 'reconstruction'")
+        elif task == "reconstruction":
             x = self.head_reconstruction(x)
             x = x.reshape((B, C, L, token.shape[-1]))
-            # (B, C, L, S), (B, C, L, S), (B, C, L)
-            return x, token, mlm_mask   # type: ignore
-        else:
+            if mask:
+                # (B, C, L, S), (B, C, L, S), (B, C, L)
+                return x, token, mlm_mask
+            else:
+                x = self.tokenizer.backward(x)
+                token = self.tokenizer.backward(token)
+                # (B, C, T), (B, C, T), None
+                return x, token, None
+        elif task == "regression":
             x = self.head_regression(x)
-            # (B, out_dim)
-            return x
+            # (B, out_dim), None, None
+            return x, None, None
