6ebb2a858a36aaba20ad2ac905896e6827e7f023
Tianrui Qi <tianrui.qi@icloud.com>
2025-10-08 19:06:35 -0400

    Implement trainer for pretrain

--- main.py
+++ main.py
@@ -1,55 +1,52 @@
 import torch
-import scipy.io
+import lightning
+import lightning.pytorch.loggers
+import lightning.pytorch.callbacks
+
+import os
+import sys
+import json
 
 import src
 
 
-waveform = scipy.io.loadmat(
-    "C:/Users/tianrui/iCloudDrive/Research/RoblyerLab/data/waveform/raw.mat"
-)["data_store"][0, 0]
-
-x = waveform[0]
-y = waveform[1]
-subject_ids = waveform[2]
-groups = waveform[3]
-repeats = waveform[4]
-conditions = waveform[5]
-
-print(subject_ids)
-print(groups)
-print(x.shape)
-print(y.shape)
-
-x = torch.from_numpy(x[0:16]).permute(0, 2, 1).float()
-# print(x.shape)
-# # plot x
-# plt.plot(x[0, 0, :])
-# plt.plot(x[0, 1, :])
-# plt.plot(x[0, 2, :])
-# plt.show()
+torch.set_float32_matmul_precision("medium")
+lightning.seed_everything(42, workers=True, verbose=False)
 
 
-D = 128
-C_max = 8
-L_max = 200
-S = 40
-stride = 20
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print(f"Using device: {device}")
+def main(cfg_load_path: str) -> None:
+    # cfg
+    with open(cfg_load_path, "r") as f: cfg = json.load(f)
+    # src
+    data = src.data.DataModule(**cfg["data"])
+    model = src.model.SCOST(**cfg["model"])
+    runner = src.runner.Pretrain(model=model, **cfg["runner"])
+    # trainer
+    logger = lightning.pytorch.loggers.TensorBoardLogger(
+        save_dir=cfg["trainer"]["log_save_fold"],
+        name=cfg_load_path.split("/")[-1].split(".")[0],
+        version="",
+    )
+    checkpoint = lightning.pytorch.callbacks.ModelCheckpoint(
+        dirpath=os.path.join(
+            cfg["trainer"]["ckpt_save_fold"], 
+            cfg_load_path.split("/")[-1].split(".")[0]
+        ),
+        monitor=cfg["trainer"]["monitor"],
+        save_top_k=cfg["trainer"]["save_top_k"],
+        save_last=True,
+    )
+    lrmonitor = lightning.pytorch.callbacks.LearningRateMonitor(
+        logging_interval="step"
+    )
+    trainer = lightning.Trainer(
+        logger=logger,
+        callbacks=[checkpoint, lrmonitor],
+        max_epochs=cfg["trainer"]["max_epochs"],
+        log_every_n_steps=cfg["trainer"]["log_every_n_steps"],
+    )
+    # fit
+    trainer.fit(runner, datamodule=data)
 
-model = src.model.SCOST(
-    D=D, S=S, stride=stride, C_max=C_max, L_max=L_max,
-    num_layers=4, nhead=8, dim_feedforward=512, out_dim=2,
-).to(device=device)
-model.train()
 
-x = x.to(device=device)
-channel_idx = torch.arange(
-    x.shape[1], device=x.device
-)[None, :].repeat(x.shape[0], 1)
-
-out, token, mlm_mask = model(x, channel_idx)
-
-model.eval()
-out = model(x, channel_idx)
-print(out.shape)
+if __name__ == "__main__": main(sys.argv[1])
