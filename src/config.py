5f6001206d342b703371543ae616a4719fe5ad13
Tianrui Qi <tianrui.qi@icloud.com>
2025-10-22 05:19:32 -0400

    Refactor config input from json to python class

---
+++ src/config.py
@@ -0,0 +1,48 @@
+__all__ = ["Config", "c01"]
+
+
+class Config:
+    def __init__(self):
+        self.data = {
+            "x_load_path": "data/waveform/x.pt",
+            "y_load_path": "data/waveform/y.pt",
+            "profile_load_path": "data/waveform/profile.csv",
+            "channel_perm": True,
+            "channel_drop": True,
+            "p_nan": 0.005,
+            "batch_size": 256,
+            "num_workers": 8
+        }
+        self.model = {
+            "D": 256,
+            "S": 40,
+            "stride": 20,
+            "C_max": 8,
+            "L_max": 1024,
+            "num_layers": 4,
+            "nhead": 8,
+            "dim_feedforward": 1024,
+            "freeze_embedding": False,
+            "freeze_transformer": 0
+        }
+        self.runner = {
+            "weight": [0.8, 0.2, 0.0, 0.0],
+            "T": 0.2,
+            "lr": 0.005,
+            "step_size": 20,
+            "gamma": 0.98
+        }
+        self.trainer = {
+            "max_epochs": 5000,
+            "log_save_fold": "log/",
+            "ckpt_save_fold": "ckpt/",
+            "monitor": "loss/valid",
+            "save_top_k": 10,
+            "ckpt_load_path": None,
+            "resume": False
+        }
+
+
+class c01(Config):
+    def __init__(self):
+        super().__init__()
