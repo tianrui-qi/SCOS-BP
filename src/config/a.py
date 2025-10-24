7652df27b651ffaf20c5f048fdb20ccfb0a7f428
Tianrui Qi <tianrui.qi@icloud.com>
2025-10-24 04:25:30 -0400

    Start trail b with new split

---
+++ src/config/a.py
@@ -0,0 +1,35 @@
+from .config import Config
+
+
+__all__ = []
+
+
+class ConfigA01(Config):
+    def __init__(self):
+        super().__init__()
+        self.data["profile_load_path"] = "data/waveform/profile.csv"
+        self.trainer["max_epochs"] = 2350
+
+
+class ConfigA02(Config):
+    def __init__(self):
+        super().__init__()
+        self.data["profile_load_path"] = "data/waveform/profile.csv"
+        self.model["freeze_embedding"] = True
+        self.model["freeze_transformer"] = 3
+        self.runner["weight"] = [0.8, 0.2, 0.0, 0.00001]
+        self.trainer["max_epochs"] = 2860
+        self.trainer["ckpt_load_path"] = "ckpt/ConfigA01/last.ckpt"
+        self.trainer["resume"] = True
+
+
+class ConfigA03(Config):
+    def __init__(self):
+        super().__init__()
+        self.data["profile_load_path"] = "data/waveform/profile.csv"
+        self.model["freeze_embedding"] = True
+        self.model["freeze_transformer"] = 2
+        self.runner["weight"] = [0.6, 0.4, 0.0, 0.0001]
+        self.trainer["max_epochs"] = 5000
+        self.trainer["ckpt_load_path"] = "ckpt/ConfigA02/last.ckpt"
+        self.trainer["resume"] = True
