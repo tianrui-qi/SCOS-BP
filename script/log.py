595854932fcb9a8cf3cbf6c4dc3853321a27835b
Tianrui Qi <tianrui.qi@icloud.com>
2025-10-26 03:52:34 -0400

    Implement helper script for post processing TensorBoard logs

---
+++ script/log.py
@@ -0,0 +1,93 @@
+import torch.utils.tensorboard
+from tensorboard.backend.event_processing import event_accumulator
+
+import os
+import glob
+import argparse
+
+
+def main() -> None:
+    # get log_load_fold
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--log_load_fold", '-l', type=str, required=True)
+    args = parser.parse_args()
+    log_load_fold = args.log_load_fold
+    # if train or valid subfolder exists, skip
+    if os.path.exists(os.path.join(log_load_fold, "train")) or \
+    os.path.exists(os.path.join(log_load_fold, "valid")):
+        print(
+            f"Subfolder 'train' or 'valid' already exist in {log_load_fold}."
+            "Skipping tensorboard log rewriting."
+        )
+        return
+    # write
+    writeEvent(
+        log_load_fold, os.path.join(log_load_fold, "train"), 
+        rename_rules = {
+            "loss/reconstruction/train" : "loss/reconstruction",
+            "loss/contrastive/train"    : "loss/contrastive",
+            "loss/regression/train"     : "loss/regression",
+            "loss/train"                : "loss",
+        }
+    )
+    writeEvent(
+        log_load_fold, os.path.join(log_load_fold, "valid"), 
+        rename_rules = {
+            "loss/reconstruction/valid" : "loss/reconstruction",
+            "loss/contrastive/valid"    : "loss/contrastive",
+            "loss/regression/valid"     : "loss/regression",
+            "loss/valid"                : "loss",
+        }
+    )
+    writeEvent(
+        log_load_fold, log_load_fold, 
+        rename_rules = {
+            "epoch"  : "lightning/epoch",
+            "lr-Adam": "lightning/lr-Adam",
+        }
+    )
+    # remove
+    os.remove(findEvent(log_load_fold))
+    if os.path.exists(os.path.join(log_load_fold, "hparams.yaml")):
+        os.remove(os.path.join(log_load_fold, "hparams.yaml"))
+
+
+def writeEvent(
+    log_load_fold: str, log_save_fold: str,
+    rename_rules: dict[str, str]
+) -> None:
+    # load old log
+    log_load_path = findEvent(log_load_fold)
+    ea = event_accumulator.EventAccumulator(log_load_path)
+    ea.Reload()
+    # save new log
+    os.makedirs(log_save_fold, exist_ok=True)
+    writer = torch.utils.tensorboard.SummaryWriter(     # type: ignore
+        log_save_fold
+    )
+    for tag in ea.Tags().get("scalars", []):
+        if tag not in rename_rules: continue
+        new_tag = rename_rules.get(tag, tag)
+        for scalar_event in ea.Scalars(tag):
+            writer.add_scalar(
+                new_tag,
+                scalar_event.value,
+                global_step=scalar_event.step,
+                walltime=scalar_event.wall_time,
+            )
+    writer.close()
+
+
+def findEvent(log_load_fold: str) -> str:
+    candidates = glob.glob(
+        os.path.join(log_load_fold, "events.out.tfevents.*")
+    )
+    if not candidates: raise FileNotFoundError(
+        f"No TensorBoard event file found in: {log_load_fold}"
+    )
+    # find the oldest one
+    candidates.sort(key=os.path.getmtime, reverse=False)
+    return candidates[0]
+
+
+if __name__ == "__main__": main()
